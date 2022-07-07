9ebdb5990affb9675d84cf8fff30930943b4ce07
Le Dien Hung <hunglpd@sphoton.com>
2022-07-07 16:35:47 +0700

    update

--- App.js
+++ App.js
@@ -1,13 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import io from 'socket.io-client';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const socket = io("192.168.0.28:3000"); // replace with the IP of your server, when testing on real devices
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      connected: socket.connected,
+      currentTransport: socket.connected ? socket.io.engine.transport.name : '-',
+      lastMessage: ""
+    };
+  }
+
+  componentDidMount() {
+    socket.on('connect', () => this.onConnectionStateUpdate());
+    socket.on('disconnect', () => this.onConnectionStateUpdate());
+    socket.on('message', (content) => this.onMessage(content));
+  }
+
+  componentWillUnmount() {
+    socket.off('connect');
+    socket.off('disconnect');
+    socket.off('message');
+  }
+
+  onConnectionStateUpdate() {
+    this.setState({
+      connected: socket.connected,
+      currentTransport: socket.connected ? socket.io.engine.transport.name : '-'
+    });
+    if (socket.connected) {
+      socket.io.engine.on('upgrade', () => this.onUpgrade());
+    } else {
+      socket.io.engine.off('upgrade');
+    }
+  }
+
+  onMessage(content) {
+    this.setState({
+      lastMessage: content
+    });
+  }
+
+  onUpgrade() {
+    this.setState({
+      currentTransport: socket.io.engine.transport.name
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>State: { this.state.connected ? 'Connected' : 'Disconnected' }</Text>
+        <Text>Current transport: { this.state.currentTransport }</Text>
+        <Text>Last message: { this.state.lastMessage }</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
